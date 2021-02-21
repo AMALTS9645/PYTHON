1708ce18861265e021dcee6c12dbf3d49a283940
AMALTS9645 <amaltsthekkel@gmail.com>
2021-02-22 00:05:31 +0530

    pcycle 3 errors fixed

--- python c3.py
+++ python c3.py
@@ -57,11 +57,11 @@ print(a)
 
 """**5.Display the given pyramid with step number accepted from user. Eg: N=4 1 2 4 3 6 9 4 8 12 16**"""
 
-rows=5
-for row in range(1,rows+1):
-  for column in range (1,row+1):
-    print(column,end='')
-  print("")
+rows = int(input("Enter the number of rows: "))  
+for i in range(1, rows):  
+    for j in range(1, i + 1):          
+        print(i * j, end='  ')  
+    print()
 
 """**6.Count the number of characters (character frequency) in a string.**"""
 
